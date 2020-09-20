6dc6233bdbeb14fdcbae5e23f2dc443b2e50cbda
Dark-Byte <darkbyte420@gmail.com>
2020-09-20 11:55:19 +0530

    made json file as password database.

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 from cryptography.fernet import Fernet
+import json
 
-key = Fernet.generate_key()
-
-print(key)
-
+ekey = open('ekey.txt', 'rb')
+key = ekey.read()
 cipher = Fernet(key)
 
-text = cipher.encrypt(b'This is encrypted')
-print(text)
 
-original_text = cipher.decrypt(text)
-print(original_text.decode())
+with open('db.json', 'r') as f:
+    db = json.load(f)
 
-db = {}
 
 def main(site, username, password):
     pw = cipher.encrypt(bytes(password, 'utf-8'))
-    db[site] = [username, pw]
-    read(site)
+    db[site] = [username, str(pw)]
+    print(db)
+    with open('db.json', 'w') as outfile:
+        json.dump(db, outfile)
+    # read(site)
 
 def read(site):
     username = db[site][0]
-    pw = cipher.decrypt(db[site][1])
+    pw = cipher.decrypt(eval(db[site][1]))
     print(username)
     print(pw.decode())
-
-if __name__ == "__main__":
-    main("instagram", "dark-byte", "password")
